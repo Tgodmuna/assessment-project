94053d7f9fbb6c142bbadcc51039f0037b0785c0
ThankGod Munachimso Agu <AguthankGod@gmail.com>
2024-01-17 09:54:34 +0100

    fix: image load lazily

--- src/components/Hero_section/InvestmentImage.tsx
+++ src/components/Hero_section/InvestmentImage.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const InvestmentImage = () => {
   return (
-    <div className="w-[70vw] object-contain mt-[2.5rem] p-2">
-      <img src='/images/investment.jpeg' alt='investment and trends' className=" shadow-md shadow-slate-500 w-full " />
+    <div className='w-[70vw] object-contain mt-[2.5rem] p-2'>
+      <img
+        loading='lazy'
+        src='/images/investment.jpeg'
+        alt='investment and trends'
+        className=' shadow-md shadow-slate-500 w-full '
+      />
     </div>
   );
 };
