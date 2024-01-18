30c6a78a049f7cfdfb14d1273149589abfbfb7eb
ThankGod Munachimso Agu <AguthankGod@gmail.com>
2024-01-18 18:54:52 +0100

    fix: made Footer data a single source of data for the footer components,

---
+++ src/components/Footer/footerData.tsx
@@ -0,0 +1,39 @@
+export const investmentTopics = [
+  "Automated Investing",
+  "Stock Investing",
+  "Explore all investments",
+  "Retirement",
+  "College",
+  "Socially Responsible",
+  "Pricing",
+  "Tax-loss Harvesting",
+  "Historical Performance",
+  "Whitepapers",
+];
+
+export const financialGuides = [
+  "Blog",
+  "Help Center",
+  "What level of risk suits you best?",
+  "Home Planning Guide",
+  "Financial Health Guide",
+  "Equity and IPO Guide",
+  "IRA Contributions Calculator",
+];
+
+export const companyInfo = [
+  "About Us",
+  "Newsroom",
+  "Reviews",
+  "Careers",
+  "Legal",
+  "Sitemap",
+  "Back to top",
+];
+
+export const BondTopic = ["Bond"];
+
+export const BorrowTopic = ["Borrow"];
+
+export   const SavingTopic = ["cash", "ATM locator"];
+
