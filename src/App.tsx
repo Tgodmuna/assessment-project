0ad4c15977e6f5b34bfdcdc64effeda4bb14311c
ThankGod Munachimso Agu <AguthankGod@gmail.com>
2024-01-18 18:56:04 +0100

    Add context  hook for to manage the hovering of image

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
+import { createContext } from "vm";
 import Automation from "./components/Automation/Automation";
 import AboutDelievry from "./components/ClientSection/AboutDelievry";
 import Client from "./components/ClientSection/CLIENT";
 import CLIENTDiscounts from "./components/ClientSection/CLIENTDiscounts";
 import Explore from "./components/Explore/Explore";
 import FAQ__UI from "./components/FAQ/FAQ__UI";
+import FOOTER from "./components/Footer/FOOTER";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero_section/Hero";
 import Info from "./components/InforSection/Info";
 import Pricing from "./components/Pricing/Pricing";
 import RiskTolerance from "./components/RiskTolerance/RiskTolerance";
+import React, { useState } from "react";
+
+//context type
+type contextType = {
+  setisFooterHovered: React.Dispatch<React.SetStateAction<boolean>>;
+  isFooterHovered: boolean;
+};
+
+//context definition
+export const Context = React.createContext<undefined | contextType>(undefined);
 
 function App() {
+  const [isFooterHovered, setisFooterHovered] = useState<boolean>(false);
+
   return (
-    <div className='App min-h-screen  '>
-      <Header />
+    <Context.Provider value={{ isFooterHovered, setisFooterHovered }}>
+      <div className='App min-h-screen  '>
+        {/* <Header />
       <Hero  />
       <RiskTolerance />
       <Explore />
@@ -23,8 +38,10 @@ function App() {
       <Client />
       <CLIENTDiscounts />
       <AboutDelievry />
-      <FAQ__UI/>
-    </div>
+      <FAQ__UI/> */}
+        <FOOTER />
+      </div>
+    </Context.Provider>
   );
 }
 
